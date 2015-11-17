a36392dfcb45e77b4122d5e929f85c9fd664b957
mingchun.or <mingchun.or@harborx.com>
2015-11-17 23:45:03 +0800

    fix last commit forgot to rm console.log

--- dist/deploy/hkg-mongo-index.js
+++ dist/deploy/hkg-mongo-index.js
@@ -1,3 +1,1 @@
-console.log("----- Start building index -----");
 db.hkg.post.createIndex({"messageId" : 1, "currentPages" : 1}, {"background":true,"unique": true, "sparse": true});
-console.log("----- Finish building index -----");
