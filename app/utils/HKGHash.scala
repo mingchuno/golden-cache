503bfa97c747df9d9f37ebc2f8a2da7a1e43ce62
Ryan Leung <rwcleung@gmail.com>
2016-04-07 17:24:04 +0800

    Add hash for topics.  And fix the `MD5` method when 0 is the starting characters

--- app/utils/HKGHash.scala
+++ app/utils/HKGHash.scala
@@ -4,10 +4,7 @@ import play.api.Logger
 
 object HKGHash {
   def md5(s: String) = {
-    val m = java.security.MessageDigest.getInstance("MD5")
-    val b = s.getBytes("UTF-8")
-    m.update(b, 0, b.length)
-    new java.math.BigInteger(1, m.digest()).toString(16)
+    java.security.MessageDigest.getInstance("MD5").digest(s.getBytes).map("%02x".format(_)).mkString
   }
 
   def getHash(forum:String, pageNum:Int, filterMode:String, sensorMode:String) = {
@@ -16,4 +13,13 @@ object HKGHash {
     var md5Str:String = md5(m)
     md5Str
   }
+
+  def getTopicHash(messageId:Int, startNum:Int, userId:Int, filterMode:String, sensorMode:String) = {
+    val m: String = new java.text.SimpleDateFormat("yyyyMMdd").format(new java.util.Date) + "_HKGOLDEN_" + "%GUEST%" + "_$API#Android_1_2^" +
+      messageId + "_" + startNum + "_" + filterMode + "_" + sensorMode
+    var md5Str: String = md5(m)
+    Logger.info(s"Key: $m, MD5: $md5Str")
+    md5Str
+  }
+
 }
