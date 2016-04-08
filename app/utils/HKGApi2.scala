1bd8f389ae06aba42b0579be68b26c2a3cdd2597
Ryan Leung <rwcleung@gmail.com>
2016-04-08 11:01:19 +0800

    Move the URL generation method to `HKGApi2.scala`

--- app/utils/HKGApi2.scala
+++ app/utils/HKGApi2.scala
@@ -3,22 +3,27 @@ package utils
 import play.api.Logger
 
 object HKGApi2 {
-  def md5(s: String) = {
-    java.security.MessageDigest.getInstance("MD5").digest(s.getBytes).map("%02x".format(_)).mkString
+  private val apiEndpoint = "http://android-1-2.hkgolden.com"
+
+  def getTopicUrl (channel:String, pageNum:Int, filter:String, sensor:String):String = {
+    val key = getTopicHash(channel, pageNum, filter, "N")
+    s"$apiEndpoint/newTopics.aspx?s=$key&type=$channel&returntype=json&page=$pageNum&filtermode=$filter&sensormode=N&user_id=0&block=Y"
   }
 
-  def getHash(forum:String, pageNum:Int, filterMode:String, sensorMode:String) = {
+  def getPostUrl ():String = {
+    s"$apiEndpoint/newView.aspx"
+  }
+
+  def getTopicHash(forum:String, pageNum:Int, filterMode:String, sensorMode:String) = {
     val m:String = new java.text.SimpleDateFormat("yyyyMMdd").format(new java.util.Date) + "_HKGOLDEN_" + "%GUEST%" + "_$API#Android_1_2^" +
       forum + "_" + pageNum + "_" + filterMode + "_" + sensorMode
-    var md5Str:String = md5(m)
-    md5Str
+    Md5.hash(m)
   }
 
-  def getTopicHash(messageId:Int, startNum:Int, userId:Int, filterMode:String, sensorMode:String) = {
+  def getPostHash(messageId:Int, startNum:Int, userId:Int, filterMode:String, sensorMode:String) = {
     val m: String = new java.text.SimpleDateFormat("yyyyMMdd").format(new java.util.Date) + "_HKGOLDEN_" + "%GUEST%" + "_$API#Android_1_2^" +
       messageId + "_" + startNum + "_" + filterMode + "_" + sensorMode
-    var md5Str: String = md5(m)
-    md5Str
+    Md5.hash(m)
   }
 
 }
